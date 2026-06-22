9d458b7bdf1b432b6a870fd528cdc4ce99ba241f
iAdityaDev <adityadevsingh16@gmail.com>
2026-06-23 00:10:07 +0530

    moveit new pacakge

--- so_arm_100_moveit_config/launch/moveit_rviz.launch.py
+++ so_arm_100_moveit_config/launch/moveit_rviz.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("so_arm_100_5dof", package_name="so101_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("so_arm_100", package_name="so_arm_100_moveit_config").to_moveit_configs()
     return generate_moveit_rviz_launch(moveit_config)
